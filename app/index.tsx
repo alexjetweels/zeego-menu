190307b46883680b1c5772feb203d2904ff79551
Alex <alex@jetweels.com>
2024-09-02 20:52:54 +0700

    save image and request permission

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,8 @@ import DropDownMenu from '@/components/DropDownMenu';
 import ImageWIthContext from '@/components/ImageWIthContext';
 import { Stack } from 'expo-router';
 import { View, Text } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
+import { useEffect } from 'react';
 
 export default function App() {
   const handleDropDownSelect = (value: string) => {
--- components/ImageWIthContext.tsx
+++ components/ImageWIthContext.tsx
@@ -1,8 +1,12 @@
+import { downloadImage } from '@/utils/download';
 import React from 'react';
 import { Image, Pressable, Text } from 'react-native';
 import * as ContextMenu from 'zeego/context-menu';
 
 function ImageWIthContext() {
+  const handleDownLoad = async () => {
+    await downloadImage('https://galaxies.dev/img/projects/star-wars-api.webp');
+  };
   return (
     <ContextMenu.Root>
       <ContextMenu.Trigger>
@@ -22,8 +26,10 @@ function ImageWIthContext() {
         avoidCollisions={true}
         collisionPadding={20}
       >
-        <ContextMenu.Item key='1' onSelect={() => console.log('press items')}>
-          <ContextMenu.ItemTitle>Menu 1</ContextMenu.ItemTitle>
+        <ContextMenu.Item key='1' onSelect={handleDownLoad}>
+          <ContextMenu.ItemIcon ios={{ name: 'square.and.arrow.down' }} />
+
+          <ContextMenu.ItemTitle>Download</ContextMenu.ItemTitle>
         </ContextMenu.Item>
       </ContextMenu.Content>
     </ContextMenu.Root>
